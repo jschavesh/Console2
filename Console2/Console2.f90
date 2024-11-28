81c6aa71657b6a52693df345bdf620843414f091
Sebastian Chaves <jschavesh@gmail.com>
2024-11-28 16:16:48 -0600

    mkl lib included

--- Console2/Console2.f90
+++ Console2/Console2.f90
@@ -15,7 +15,7 @@
     PROGRAM Console2
     use mkl_dfti
     
-
+    
     IMPLICIT NONE
     
     !integer, parameter :: n = 4, m = 5
@@ -27,14 +27,20 @@
     write (*,*) 'Eneter de'
     read (*,*) theta
     
-    write(*, '(A,F10.4)') 'The secant is ', 1. / cos(theta *pi/180.)
-    !contains
-    !function secant(angle_in_degrees)
-    !
-    !
-    print *, "Hello World"
-    
+    write(*, '(A,F10.4)') 'The secant is ', secant(theta)
     pause
+    
+    
+    contains
+    real function secant(angle_in_degrees)
+    real :: angle_in_degrees
+    
+    secant = 1. / cos(theta *pi/180.)
+        
+    end function secant
+    
+    
+
 
     end PROGRAM Console2
 
